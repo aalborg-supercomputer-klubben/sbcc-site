78bd04528846b9efe5eea4b09f1430564c1b84fd
jskad <j.skadborg00@gmail.com>
2023-02-25 16:09:40 +0100

    insert participants

--- data/participants.ts
+++ data/participants.ts
@@ -6,29 +6,39 @@ type Participant = {
 
 const participants: Participant[] = [
   {
-    Name: "AAU",
+    Name: "Aalborg University",
     Url: "https://www.aau.dk/",
     Logo_path: "/img/participants/Aalborg-University-Denmark.png",
   },
   {
-    Name: "AAU",
+    Name: "ETH Zürich",
     Url: "https://www.aau.dk/",
     Logo_path: "/img/participants/ETH-Zurich_Logo_black.svg",
   },
   {
-    Name: "AAU",
-    Url: "https://www.aau.dk/",
-    Logo_path: "/img/participants/Aalborg-University-Denmark.png",
+    Name: "Friedrich-Alexander-Universität",
+    Url: "https://www.fau.de/",
+    Logo_path: "/img/participants/Friedrich-Alexander-University.svg",
   },
   {
-    Name: "AAU",
-    Url: "https://www.aau.dk/",
-    Logo_path: "/img/participants/Aalborg-University-Denmark.png",
+    Name: "MIT, Boston University",
+    Url: "https://www.mit.edu/",
+    Logo_path: "/img/participants/MIT-Massachusetts-Institute-of-Technology-Logo.png",
   },
   {
-    Name: "AAU",
-    Url: "https://www.aau.dk/",
-    Logo_path: "/img/participants/Aalborg-University-Denmark.png",
+    Name: "Northeastern University",
+    Url: "https://www.northeastern.edu/",
+    Logo_path: "/img/participants/Northeastern-University-Logo.png",
+  },
+  {
+    Name: "University of California San Diego",
+    Url: "https://ucsd.edu/",
+    Logo_path: "/img/participants/University-of-California-San-Diego.png",
+  },
+  {
+    Name: "University of Texas at Austin",
+    Url: "https://www.utexas.edu/",
+    Logo_path: "/img/participants/University-of-Texas-at-Austin.svg",
   },
 ];
 
