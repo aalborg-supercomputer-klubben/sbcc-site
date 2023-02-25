519201645b5009dde48e5283dfefea56372909cb
jskad <j.skadborg00@gmail.com>
2023-02-25 15:00:51 +0100

    made participants stuff

---
+++ data/participants.ts
@@ -0,0 +1,35 @@
+type Participant = {
+  Name: string;
+  Url: string;
+  Logo_path: string;
+};
+
+const participants: Participant[] = [
+  {
+    Name: "AAU",
+    Url: "aau.dk",
+    Logo_path: "/../public/img/participants/Aalborg-University-Denmark.png",
+  },
+  {
+    Name: "AAU",
+    Url: "aau.dk",
+    Logo_path: "/../public/img/participants/Aalborg-University-Denmark.png",
+  },
+  {
+    Name: "AAU",
+    Url: "aau.dk",
+    Logo_path: "/../public/img/participants/Aalborg-University-Denmark.png",
+  },
+  {
+    Name: "AAU",
+    Url: "aau.dk",
+    Logo_path: "/../public/img/participants/Aalborg-University-Denmark.png",
+  },
+  {
+    Name: "AAU",
+    Url: "aau.dk",
+    Logo_path: "/../public/img/participants/Aalborg-University-Denmark.png",
+  },
+];
+
+export default participants;
