4ea00c52994d3d32c5b1649752bd849c03633ef7
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-02-26 23:05:30 +0100

    navbar alignment

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,8 +19,8 @@ export default function Navbar() {
 
   return (
     <div className="w-full">
-      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
-        {/* Logo  */}
+      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-end xl:px-0">
+        {/* Burger menu  */}
         <Disclosure>
           {({ open }) => (
             <>
