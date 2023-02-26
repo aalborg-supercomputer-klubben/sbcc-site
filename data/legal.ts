3d9f8e80104e3086eed9645796ff496ae2dcffe5
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-02-26 21:21:33 +0100

    make socials render based on data. Fix content in footer

---
+++ data/legal.ts
@@ -0,0 +1,18 @@
+type Legal = {
+    name: string;
+    href: string;
+}
+export const legal: Legal[] = [
+    // {
+    //     name: "Terms",
+    //     href: "#"
+    // },
+    // {
+    //     name: "Privacy",
+    //     href: "#"
+    // },
+    // {
+    //     name: "Legal",
+    //     href: "#"
+    // }
+]
---
+++ data/socials.ts
@@ -0,0 +1,13 @@
+type Socials = {
+    twitter?: string;
+    github?: string;
+    discord?: string;
+    instagram?: string;
+    facebook?: string;
+    linkedin?: string;
+
+}
+export const socials: Socials = {
+    discord: "https://discord.gg/Z49YY5FE",
+    github: "https://github.com/SuperCompUCSD"
+}
