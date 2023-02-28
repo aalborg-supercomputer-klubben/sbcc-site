9340b0fa7a8ca152405674543ec14ccb241b9e35
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-02-28 21:16:47 +0100

    updated headers with preview

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,29 @@
 import {competition} from "../data/competition";
 import Head from "next/head";
 
+
+const path_to_logo = "https://sbcc.io/img/logo.svg";
 const Headers = () => {
     return (
         <Head>
+            {/*!--Primary Meta Tags --*/}
             <title>{competition.title}</title>
-            <meta
-                name="description"
-                content={competition.title + "is a Single Board Cluster Competition made for students by students"}
-            />
+            <meta name="title" content={competition.title}/>
+            <meta name="description" content={competition.title + "is a Single Board Cluster Competition made for students by students"}/>
 
-            {/* Open Graph / Facebook */}
+            {/*<!-- Open Graph / Facebook --> */}
             <meta property="og:type" content="website"/>
-            <meta name="og:title" content={competition.title}/>
-            <meta name="og:description"
-                  content={competition.title + "is a Single Board Cluster Competition made for students by students"}/>
-            {/* Twitter */}
+            <meta property="og:url" content="https://sbcc.io/"/>
+            <meta property="og:title" content={competition.title}/>
+            <meta property="og:description" content={competition.title + "is a Single Board Cluster Competition made for students by students"}/>
+            <meta property="og:image" content={path_to_logo}/>
+
+            {/*<!-- Twitter -->*/}
             <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content="https://sbcc.supercomputersquad.dk/"/>
-            <meta name="twitter:title" content={competition.title}/>
-            <meta name="twitter:description"
-                  content={competition.title + "is a Single Board Cluster Competition made for students by students"}/>
-
-
+            <meta property="twitter:url" content="https://sbcc.io/"/>
+            <meta property="twitter:title" content={competition.title}/>
+            <meta property="twitter:description" content={competition.title + "is a Single Board Cluster Competition made for students by students"}/>
+            <meta property="twitter:image" content={path_to_logo}/>
             {/* favicon */}
             <link rel="icon" href="/favicon.ico"/>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
