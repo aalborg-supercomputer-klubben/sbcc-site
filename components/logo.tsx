9705f3524f0808f97e21c4542e43180a72f0ba6c
jskad <j.skadborg00@gmail.com>
2023-03-01 23:25:14 +0100

    Fixed logo mounting

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 import lightLogo from "../public/img/logo.svg";
 import darkLogo from "../public/img/dark-logo.svg";
 import {competition} from "../data/competition";
+import { useEffect, useState } from "react";
 export const Logo = () => {
+    const [mounted, setMounted] = useState(false);
     const {resolvedTheme} = useTheme();
+
+    useEffect(() => setMounted(true), []);
+    if (!mounted) return null;
 
     return (
         <span className="hover:opacity-75">
