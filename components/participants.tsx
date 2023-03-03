03651cfd8e0f965bc93d64ef475d80b8a9cd501e
Anders Aaen Springborg <anders.springborg@askcody.com>
2023-03-03 17:45:14 +0100

    Revert "Merge pull request #22 from supercomputer-klubben-aau/participants-update"
    This reverts commit 0652143ed1dcc05395b7d0ce654327fcd26b7d17, reversing
    changes made to 30490ec633e4b9e2ef395bc1b19c51954f7b1382.

--- components/participants.tsx
+++ components/participants.tsx
@@ -3,26 +3,13 @@ import Link from "next/link";
 import participants from "../data/participants";
 import React from "react";
 
-const FadeIn = () => (<div className="relative w-0 h-0">
-                <div className="absolute dark:from-trueGray-900 bg-gradient-to-r from-white opacity-100 w-48 h-36 z-10 bottom-0" />
-                <div className="absolute dark:bg-trueGray-900 bg-white opacity-100 w-screen h-36 z-10 bottom-0 right-0" />
-            </div>)
-const FadeOut = () => (<div className="relative w-full">
-                <div className="absolute dark:from-trueGray-900 bg-gradient-to-l from-white opacity-100 w-48 h-36 z-10 right-0 bottom-0" />
-                <div className="absolute dark:bg-trueGray-900 bg-white opacity-100 w-screen h-36 z-10 bottom-0 left-full" />
-            </div>)
-
 export default function Participants() {
-    // duplicate all participants to make the banner infinite
-    const infiniteParticipants = [...participants, ...participants]
-
     return (
-        <>
-        <div className={"flex gap-5 mt-10 animate-infinite-banner ml-14"}>
-            {infiniteParticipants.map(p => {
+        <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
+            {participants.map(p => {
                 return (
-                    <Link key={p.Name} href={p.Url}>
-                        <div className="flex flex-col items-center justify-center gap-[28px] w-48 h-36">
+                    <Link key={p.Name} href={p.Url} className="pt-">
+                        <div className="flex flex-col items-center justify-center gap-2 w-48 h-36">
                         <Image
                             src={p.Logo_path}
                             alt={p.Name + " logo"}
@@ -34,8 +21,5 @@ export default function Participants() {
                 )
             })}
         </div>
-        <FadeIn/>
-        <FadeOut/>
-        </>
     )
 }
