7e86fd2966219fd289039f3ccb039b22156b3a51
Mads Mogensen <mail@madsmogensen.dk>
2023-10-26 16:45:42 +0200

    Define logo text with html

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 import {useTheme} from "next-themes";
 import Image from "next/image";
-import lightLogo from "../public/img/logo.svg";
-import darkLogo from "../public/img/dark-logo.svg";
+import lightLogo from "../public/img/logo-no-text.svg";
+import darkLogo from "../public/img/dark-logo-no-text.svg";
 import {competition} from "../data/competition";
 import { useEffect, useState } from "react";
 
@@ -13,17 +13,19 @@ export const Logo = () => {
     if (!mounted) return null;
     
     return (
-        <span className="hover:opacity-75">
+        <span className="hover:opacity-75 flex items-center">
             {resolvedTheme === "dark" ? (
                 <Image
                     src={darkLogo}
                     alt={competition.title + " dark logo"}
-                    className="w-36"
-                />) : (
+                    className="w-24"/>) : (
                     <Image
                     src={lightLogo}
                     alt={competition.title + " logo"}
-                    className="w-36"
-                />)}
+                    className="w-24"/>)}
+            <div className="flex flex-col custom-color">
+              <p>SBCC</p>
+              <p>2024</p>
+            </div>
         </span>)
 };
